60f8cbaf8793bbdd2fcf69936ff121334996c23f
sparksys98 <hanineiroukh@gmail.com>
2020-02-04 11:29:39 +0200

    Switched from Python 2 to Python 3

--- madlibs.py
+++ madlibs.py
@@ -3,16 +3,19 @@ def main():
 	print("Mad libs where libs get mad.")
 	print("Start below:")
 	print("")
-	time=raw_input("Enter a number from 1 to 12: ")
-	items=raw_input("Enter a noun (plural): ")
-	name=raw_input("Enter a name: ").capitalize()
-	scream=raw_input("Enter any sentnce: ").upper()
-	action=raw_input("shake my head: ")
+	time=input("Enter a number from 1 to 12: ")
+	items=input("Enter a noun (plural): ")
+	name=input("Enter a name: ").capitalize()
+	scream=input("Enter any sentnce: ").upper()
+	action=input("Enter any action: ")
 
 	print("The story goes...")
+    print("")
 
-	print('''It was %s o'clock when I heard a knock at the door.
-	I opened the door and there was a box full of %s with a note saying "From Mr. %s."
-	Just as I closed the door I heard a scream "%s."
-	I froze in place and all I could do was %s.''') % (time,items,name,scream,action)
+
+	result=('''It was {} o'clock when I heard a knock at the door.
+	I opened the door and there was a box full of {} with a note saying "From Mr. {}."
+	Just as I closed the door I heard a scream "{}."
+	I froze in place and all I could do was {}.''').format(time,items,name,scream,action)
+	print(result)
 main()
